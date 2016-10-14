d28bead5b7fbe121d09aceca80c4b3a613581532
miracleren <miracleren@californiacolleges.net>
2016-10-14 12:50:21 +0800

    Update ABCSpam.php

--- ABCSpam.php
+++ ABCSpam.php
@@ -11,7 +11,7 @@ Description: Wordpress的垃圾评论中有95%是全英文评论和含有日文�
 function bbSpamStopCheck( $comment ) { 
 	$pattern = '/[一-龥]/u'; 
 	// 禁止全英文评论 
-	$status = preg_match_all($pattern, $comment['comment_content'],$match);
+	$status = preg_match_all(utf8_encode($pattern), utf8_encode($comment['comment_content']),$match);
 	if(!$status) { 
 		$mess = 'You should type some Chinese word (like 支持 ) in your comment to pass the spam-check, thanks for your patience! 您的评论中必须包含汉字!'; 
         	wp_die($mess . '<br /><a href="' . $_SERVER['HTTP_REFERER'] .'#respond">返回</a>'.$status.utf8_encode($comment['comment_content']));
